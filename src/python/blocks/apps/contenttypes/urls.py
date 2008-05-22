80347fc675dc9e93ae2ba9f941f5c135e984cbbf
kimus.linuxus <kimus.linuxus@a022e77c-d149-0410-bc0c-ab3f8ec7a10b>
2008-05-22 22:56:50 +0000

    support for static pages

---
+++ src/python/blocks/apps/contenttypes/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls.defaults import *
+    
+urlpatterns = patterns('blocks.apps.contenttypes.views',
+    (r'^(?P<url>.*)$', 'staticpage'),
+)
---
+++ src/python/blocks/apps/contenttypes/views.py
@@ -0,0 +1,37 @@
+from django.template import loader, RequestContext
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.conf import settings
+from django.utils.safestring import mark_safe
+from django.core.xheaders import populate_xheaders
+
+from blocks.apps.contenttypes.models import StaticPage
+
+DEFAULT_TEMPLATE = 'templates/default.html'
+
+def staticpage(request, url):
+    if not url.startswith('/'):
+        url = "/" + url
+    f = get_object_or_404(StaticPage, url__exact=url)
+    
+    # If registration is required for accessing this page, and the user isn't
+    # logged in, redirect to the login page.
+    #if f.registration_required and not request.user.is_authenticated():
+    #    from django.contrib.auth.views import redirect_to_login
+    #    return redirect_to_login(request.path)
+    if f.template:
+        t = loader.select_template((f.template.template, DEFAULT_TEMPLATE))
+    else:
+        t = loader.get_template(DEFAULT_TEMPLATE)
+
+    # To avoid having to always use the "|safe" filter in flatpage templates,
+    # mark the title and content as already safe (since they are raw HTML
+    # content in the first place).
+    f.title = mark_safe(f.title)
+    #f.content = mark_safe(f.content)
+
+    c = RequestContext(request, {'page': f, })
+    
+    response = HttpResponse(t.render(c))
+    populate_xheaders(request, response, StaticPage, f.id)
+    return response
