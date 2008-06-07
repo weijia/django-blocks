f872a43ba68df6d59fad3fcf817bfb1ca37505b5
kimus.linuxus <kimus.linuxus@a022e77c-d149-0410-bc0c-ab3f8ec7a10b>
2008-06-07 01:16:12 +0000

    personal site sample

---
+++ samples/personal-site/personal/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
+
+urlpatterns = []
+
+if settings.MEDIA_URL.startswith('/'):
+	media_url = settings.MEDIA_URL
+	if media_url.startswith('/'):
+	   media_url = media_url[1:]
+	if media_url.endswith('/'):
+	   media_url = media_url[:-1]
+	urlpatterns += patterns('',
+        (r'^%s/(?P<path>.*)$' % (media_url), 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
+
+urlpatterns += patterns('',
+	(r'^admin/(.*)', admin.site.root),
+	(r'', include('blocks.apps.contenttypes.urls')),
+)
