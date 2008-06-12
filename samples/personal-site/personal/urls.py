625cae5cbbb2acbb91f7179fdb1a0943d856a17c
kimus.linuxus <kimus.linuxus@a022e77c-d149-0410-bc0c-ab3f8ec7a10b>
2008-06-12 00:00:02 +0000

    blog comments initial support

--- samples/personal-site/personal/urls.py
+++ samples/personal-site/personal/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 #from django.contrib import admin
 
+from django.contrib.comments.feeds import LatestFreeCommentsFeed
+from django.contrib.comments.models import FreeComment
+
 urlpatterns = []
 
 if settings.MEDIA_URL.startswith('/'):
@@ -14,11 +17,22 @@ if settings.MEDIA_URL.startswith('/'):
         (r'^%s/(?P<path>.*)$' % (media_url), 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
 
+comments_info_dict = {
+    'queryset': FreeComment.objects.filter(is_public=True),
+    'paginate_by': 15,
+}
+
+feeds = { 'rss': LatestFreeCommentsFeed, }
+
 urlpatterns += patterns('',
 	#(r'^admin/(.*)', admin.site.root),
 	(r'^admin/', include('django.contrib.admin.urls')),
 
     (r'^blog/', include('blocks.apps.blog.urls')),
 	(r'^feeds/', include('blocks.apps.aggregator.urls')),
+	
+	url(r'^comments/$', 'django.views.generic.list_detail.object_list', comments_info_dict, 'comments-list'),
+	(r'^comments/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+   
 	(r'', include('blocks.apps.contenttypes.urls')),
 )
