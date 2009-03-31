9a532e168ec1a1b4facd726adc0cf14e0ff808b5
kimus.linuxus <kimus.linuxus@a022e77c-d149-0410-bc0c-ab3f8ec7a10b>
2009-03-31 21:47:17 +0000

    some minor fixes. initial implementation of the fit option on the ImageField

--- trunk/src/python/blocks/apps/search/crawlers/model.py
+++ trunk/src/python/blocks/apps/search/crawlers/model.py
@@ -9,7 +9,7 @@ class ModelCrawler(Crawler):
     def crawl(self):
         models = site.get_registered_models()
         for model, search_options in models:
-            ct = ContentType.objects.get_for_model(model)
+            #ct = ContentType.objects.get_for_model(model)
             for instance in search_options.manager.all():
                 search_options.instanciate(instance)
                 self.backend.update(search_options.guid, search_options.fields)
