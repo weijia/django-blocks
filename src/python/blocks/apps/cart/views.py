ea83de42088bdc28ccc3953f658a13a4a7c0bbb1
kimus.linuxus <kimus.linuxus@a022e77c-d149-0410-bc0c-ab3f8ec7a10b>
2009-06-11 00:46:20 +0000

    initial cart checkout support

--- src/python/blocks/apps/cart/views.py
+++ src/python/blocks/apps/cart/views.py
@@ -1,6 +1,9 @@
 from django.views.generic.simple import direct_to_template
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 from blocks.apps.cart import Cart
 
@@ -34,8 +37,34 @@ def cart_remove_item(request, queryset, object_id):
     cart.remove(obj)
     return HttpResponseRedirect(reverse('blocks.cart_detail'))
 
-def cart_checkout(request, queryset):
-    pass
+@login_required
+def cart_checkout(request):
+    user = request.user
+    try:
+        profile = user.get_profile()
+        return direct_to_template(request, template='cart/checkout_confirm.html', extra_context={'cart':  Cart(request), 'profile': profile, })
+    except ObjectDoesNotExist:
+        from profiles import utils
+        
+        form_class = utils.get_profile_form()
+        if request.method == 'POST':
+            form = form_class(data=request.POST, files=request.FILES)
+            if form.is_valid():
+                profile_obj = form.save(commit=False)
+                profile_obj.user = request.user
+                profile_obj.save()
+                if hasattr(form, 'save_m2m'):
+                    form.save_m2m()
+                return HttpResponseRedirect(reverse('blocks.cart_checkout'))
+        else:
+            form = form_class()
+        return direct_to_template(request, template='cart/checkout_needsprofile.html', extra_context={'form':  form,})
+
+@login_required
+def cart_confirm(request):
+    cart = Cart(request)
+    cart.checkout(request)
+    return direct_to_template(request, template='cart/checkout_complete.html')
 
 def cart_detail(request, queryset):
-    return direct_to_template(request, template='blocks/cart_detail.html', extra_context={'cart':  Cart(request),})
+    return direct_to_template(request, template='cart/cart_detail.html', extra_context={'cart':  Cart(request),})
