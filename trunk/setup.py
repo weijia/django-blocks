587d2fdfe4f21470d4494fd3fac9ab6b2a9d91cb
kimus.linuxus@gmail.com <kimus.linuxus@gmail.com@a022e77c-d149-0410-bc0c-ab3f8ec7a10b>
2014-03-04 17:06:05 +0000

    changed dependencies

--- trunk/setup.py
+++ trunk/setup.py
@@ -7,19 +7,30 @@ SRC_PATH = 'src/python'
 setup(
 	name = "django-blocks",
 	version = "0.1",
-
 	packages = find_packages(SRC_PATH),
 	package_dir = { '': SRC_PATH, },
-	include_package_data = True,	# include everything in source control
-
+	include_package_data = True,    # include everything in source control
+	
 	# blog still uses reStructuredText, so ensure that the docutils and Pygments get installed
-	install_requires = ['PIL', 'docutils', 'Pygments'],
-
-
+	install_requires = ['Django'],
+	
 	# metadata for upload to PyPI
 	author = "kimus",
 	author_email = "kimus.linuxus@gmail.com",
 	description = "Django Blocks will provide an easier way to build Web apps more quickly and with almost no code. ",
 	license = "MIT License",
 	url = "http://code.google.com/p/django-blocks/",
+	download_url='http://code.google.com/p/django-blocks/downloads/detail?name=django-blocks-snapshot-r244.tar.gz',
+	classifiers=['Development Status :: 4 - Beta',
+		           'Environment :: Web Environment',
+		           'Framework :: Django',
+		           'Intended Audience :: Developers',
+		           'License :: OSI Approved :: MIT License',
+		           'Operating System :: OS Independent',
+		           'Programming Language :: Python',
+		           'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+		           'Topic :: Software Development',
+		           'Topic :: Software Development :: Libraries :: Application Frameworks',
+	],
 )
+
